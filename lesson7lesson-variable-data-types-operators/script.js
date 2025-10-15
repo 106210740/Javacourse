a7b411c7ba78a3d1bfd4dc050ea46eb0fddd82a3
Nguyễn Tuấn Anh <anhmc@mkus-laptop.local>
2025-10-15 20:54:10 +0700

    update

---
+++ lesson7lesson-variable-data-types-operators/script.js
@@ -0,0 +1,15 @@
+//var, let, const
+var Ten = "Tuan Anh";
+var tuoi = 18;
+var hocSinh = true;
+
+console.log(hoTen);
+console.log(tuoi);
+console.log(hocSinh);
+
+//Cú pháp hiện đại ngày nay (let, const)
+let FullName = "Nguyen Tuan Anh";
+FullName ="Nguyen Tuan Anh"
+
+//template literal `````
+//const firstName:string = "Tuan"; 
