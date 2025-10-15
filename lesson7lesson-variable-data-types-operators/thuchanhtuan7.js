59e54e6190bad67ec6d9296cda562e30d7287492
Nguyễn Tuấn Anh <anhmc@mkus-laptop.local>
2025-10-15 21:21:23 +0700

    thuchanhtuan7

---
+++ lesson7lesson-variable-data-types-operators/thuchanhtuan7.js
@@ -0,0 +1,11 @@
+
+inputWidth = prompt("Width?");
+inputHeight = prompt("Height?");
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
+let area = width * height;
+document.write("Enter Width: " + '</br>' + width);
+document.write('</br>')
+document.write("Enter Height" + '</br>' + height);
+document.write('</br>')
+document.write("The area is:" + area);
