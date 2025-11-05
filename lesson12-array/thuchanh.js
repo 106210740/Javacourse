4219e18076b13990dfcc96e9287fd1c019239368
Nguyễn Tuấn Anh <anhmc@mkus-laptop.local>
2025-11-05 20:39:48 +0700

    bai 2

--- lesson12-array/thuchanh.js
+++ lesson12-array/thuchanh.js
@@ -1,16 +1,29 @@
-// Bai 1
-let x = 0;
-let array = Array();
-function add_element_to_array() {
-    array[x] = document.getElementById("txtValue").value;
-    alert("Element: " + array[x] + "Added at index " + x);
-    x++;
-    document.getElementById("txtValue").value = "";
+// // Bai 1
+// let x = 0;
+// let array = Array();
+// function add_element_to_array() {
+//     array[x] = document.getElementById("txtValue").value;
+//     alert("Element: " + array[x] + "Added at index " + x);
+//     x++;
+//     document.getElementById("txtValue").value = "";
+// }
+// function display_array() {
+//     let e = "<hr/>";
+//     for (let i = 0; i < array.length; i++) {
+//         e += "Element" + i + "=" + array[i] + "<br/>";
+//     }
+//     document.getElementById("result").innerHTML = e;
+// }
+
+// Bai 2
+let x = [-3, 5, 1, 3, 2, 10];
+let first = 0;
+let last = x.length - 1;
+while (first<last) {
+    let b = x[first];
+    x[first] = x[last];
+    x[last] = b;
+    first++;
+    last--;
 }
-function display_array() {
-    let e = "<hr/>";
-    for (let i = 0; i < array.length; i++) {
-        e += "Element" + i + "=" + array[i] + "<br/>";
-    }
-    document.getElementById("result").innerHTML = e;
-}
+document.write(x);
